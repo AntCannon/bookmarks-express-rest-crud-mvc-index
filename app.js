9eef7a7dd8556ee726a17e31cbeb211f5628958c
AntCannon <anthonycannonier@gmail.com>
2024-06-23 17:13:23 -0400

    add 404 route

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ app.get("/", (req, res) => {
   res.send("Welcome to Bookmarks App");
 });
 
+// bookmarks
 const bookmarksController = require("./controllers/bookmarksController.js");
 app.use("/bookmarks", bookmarksController);
+
+// 404 page
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
 
 // EXPORT
 module.exports = app;
